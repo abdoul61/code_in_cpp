a2efde9ab5b911f7c4cad1918eacaf3b335cc5a3
Abdoul <azizamine61@yahoo.fr>
2024-05-26 22:11:34 -0400

    added set operations

---
+++ arrayAdtCpp/setOperatons/code.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+
+struct Array {
+	int A[10];
+	int length;
+	int size;
+};
+
+int main(){
+	
+	printf("It is working.... ");
+	return  0;
+}
