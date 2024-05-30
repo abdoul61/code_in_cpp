146ae8690b3fb7ad77e517ee4e19f771efe3e278
Abdoul <azizamine61@yahoo.fr>
2024-05-29 20:11:35 -0400

    Added the Intersection function

--- arrayAdtCpp/setOperatons/code.cpp
+++ arrayAdtCpp/setOperatons/code.cpp
@@ -43,6 +43,32 @@ struct Array* Union(struct Array *r1,struct Array *r2){
 	result->size = 10;
 	return result;
 }
+// This function perform an intersection between two arrays
+struct Array* Intersection(struct Array *r1,struct Array *r2){
+	struct Array *result = (struct Array *)malloc(sizeof(struct Array));
+	int i,j,k;
+	i=j=k=0;
+	while(i < r1->length && j < r2->length){
+		// In this case i should only copy one element if they are both equal
+		if(r1->A[i] < r2->A[j]){
+			// element not equal; so not copy
+			i++;
+
+		}else if(r2->A[j] < r1->A[i]){
+			// element not equal; so not copy
+			 j++;
+			// if there are both the same, then add one of them and increment the other pointer as well
+		}else{
+			result->A[k++] = r2->A[j++];
+			i++;
+
+		}
+	}
+
+	result->length = k;
+	result->size = 10;
+	return result;
+}
 
 // function to display
 void Display(struct Array array){
@@ -55,10 +81,14 @@ int main(){
 	struct Array r = {{2,6,10,15,25},5,10};
 	struct Array r2 = {{3,6,7,15,20},5,10};
 	struct Array *r3;
+	struct Array *r4;
 	Display(r);
 	r3 = Union(&r,&r2);
 	printf("The union of r1 and r2 is\n");
 	Display(*r3);
+	r4 = Intersection(&r,&r2);
+	printf("The intersection of r1 and r2\n");
+	Display(*r4);
 	return  0;
 }
 
